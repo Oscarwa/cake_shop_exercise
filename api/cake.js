dc707e64206e9ea2a39b9cba175d51c284271415
Oscar Barba <racso_181088@hotmail.com>
2020-11-10 11:23:00 -0600

    - Added validation middleware - removed postman collection from repo

--- api/cake.js
+++ api/cake.js
@@ -2,57 +2,75 @@ const express = require('express');
 const router = express.Router();
 const cakeService = require('../service/cake')
 
-router.get('/', async (req, res) => {
+const cakeValidation = (req, res, next) => {
+    const idParam = req.params.id;
+    if(idParam) {
+        req.params.id = idParam.toString().toLowerCase().split(' ').join('-');
+    }
+
+    const cakeModel = req.body;
+    if(Object.keys(cakeModel).length !== 0) {
+        console.log('has cake model', cakeModel);
+        if(cakeModel.name && cakeModel.price !== undefined && cakeModel.flavor) {
+            next();
+        } else {
+            res.status(400).json({error: 'You must include a "name", "price" and "flavor" property'})
+        }
+    }
+    next();
+}
+
+router.get('/', cakeValidation, async (req, res) => {
     const cakes = await cakeService.getAll();
     res.json(cakes);
 });
-router.get('/:id', async (req, res) => {
-    const idParam = req.params.id;
-    const id = idParam.toString().toLowerCase().split(' ').join('-');
+router.get('/:id', cakeValidation, async (req, res) => {
+    const id = req.params.id;
     const cake = await cakeService.get(id);
     if(cake) {
         res.json(cake);
+    } else {
+        res.status(404).json({error:'Cake not found'});
     }
-    res.status(404).send('Cake not found');
 });
 
-router.post('/', async (req, res) => {
+router.post('/', cakeValidation, async (req, res) => {
     const model = req.body;
     
     if(model.name) { 
         const key = model.name.toString().toLowerCase().split(' ').join('-');
         const cake = await cakeService.get(key);
         if(cake) {
-            res.status(400).send(`duplicated cake key`, key);
+            res.status(400).json({error: `duplicated cake key: ${key}`});
         } else {
             const createdCake = await cakeService.create(key, model);
             res.json(createdCake);
         }
+    } else {
+        res.status(400).json({error:'You need to include at least a "name" property'});
     }
-    res.status(400).send('You need to include at least a "name" property');
 });
 
-router.delete('/:id', async (req, res) => {
-    const idParam = req.params.id;
-    const id = idParam.toString().toLowerCase().split(' ').join('-');
+router.delete('/:id', cakeValidation, async (req, res) => {
+    const id = req.params.id;
 
     const cakeRes = await cakeService.remove(id);
     if(cakeRes) {
-        res.json("success")
+        res.json({message: 'success'});
+    } else {
+        res.status(404).json({error: 'Cake not found'})
     }
-    res.status(404).send('Cake not found');
 });
 
-router.patch('/:id', async (req, res) => {
-    const idParam = req.params.id;
-    const id = idParam.toString().toLowerCase().split(' ').join('-');
+router.patch('/:id', cakeValidation, async (req, res) => {
+    const id = req.params.id;
     const cake = await cakeService.get(id);
     if(cake) { 
         const model = req.body;
         const updatedCake = await cakeService.update(id, model);
         res.json(updatedCake);
     } else {
-        res.status(404).send('Cake not found');
+        res.status(404).json({error:'Cake not found'});
     }
 
 });
